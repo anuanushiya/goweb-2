810449623f9f1079aa2ecdaadfbf86f12e4d0e15
qiwihui <qwh005007@gmail.com>
2015-06-16 10:30:19 +0800

    upload file

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
     "crypto/md5"
     "io"
     "strconv"
+    "os"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request){
@@ -51,9 +52,41 @@ func login(w http.ResponseWriter, r *http.Request){
     }
 }
 
+func upload(w http.ResponseWriter, r *http.Request){
+    fmt.Println("method:", r.Method) //获取请求的方法
+    if r.Method == "GET" {
+        crutime := time.Now().Unix()
+        h := md5.New()
+        io.WriteString(h, strconv.FormatInt(crutime, 10))
+        token := fmt.Sprintf("%x", h.Sum(nil))
+
+        t, _ := template.ParseFiles("upload.gtpl")
+        t.Execute(w, token)
+    } else {
+	r.ParseMultipartForm(32 << 20)
+	// get file handler
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+	    fmt.Println(err)
+	    return
+	}
+	defer file.Close()
+	fmt.Fprintf(w, "%v", handler.Header)
+	f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+	    fmt.Println(err)
+	    return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+
+    }
+}
+
 func main(){
     http.HandleFunc("/", sayhelloName)
     http.HandleFunc("/login", login)
+    http.HandleFunc("/upload", upload)
     err := http.ListenAndServe(":9090", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
